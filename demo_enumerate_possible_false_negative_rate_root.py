4841c8abdb89180a06f431c68d7541a8d56153d6
xq-growing <15709991203@163.com>
2022-08-21 15:13:08 +0800

    modify loop number can formate a compress curved surface, Meanwhile, just add head title for this image.

--- demo_enumerate_possible_false_negative_rate_root.py
+++ demo_enumerate_possible_false_negative_rate_root.py
@@ -7,27 +7,31 @@ from mpl_toolkits.mplot3d import Axes3D
 import time
 
 def compute_and_judge():
-    x = 0.8
-    y = 0
-    z = 0.8
+    # 物理域误报率
+    u = 0.15
+    # 控制域误报率
+    v = 0
+    # 信息域误报率
+    w = 0.15
     n = 1
     root = []
     root_list = []
     three_metrics_target = []
     while n < 30000:
         root = []
-        if (1-x)*(1-y)*(1-z)<= 0.001:
-            if 0 < x-z <1 and 0 < x-y <1 and z>= 0.8 and 0 < x < 1 :
-                target_metrics = 1-(1-x)*(1-y)*(1-z)
-                root.append(x)
-                root.append(y)
-                root.append(z)
-                print('root',root)
+        if (1-u)*(1-v)*(1-w) >= 0.85:
+            if 0.15 >= w > u > 0 and v > u > 0 and 0 < v < 1 :
+                target_metrics = 1-(1-u)*(1-v)*(1-w)
+                root.append(u)
+                root.append(v)
+                root.append(w)
+                if root[2] > 0.1:
+                    print('root',root)
                 root_list.append(root)
                 three_metrics_target.append(target_metrics)
-        x = float(random.choice(np.arange(0.8,1,0.001)))
-        y = float(random.choice(np.arange(0,1,0.0001)))
-        z = float(random.choice(np.arange(0.8,1,0.001)))
+        u = float(random.choice(np.arange(0,0.15,0.001)))
+        v = float(random.choice(np.arange(0,1,0.001)))
+        w = float(random.choice(np.arange(0,0.15,0.001)))
         # y+=0.01
         # z+=0.01
         n+=1
@@ -39,20 +43,21 @@ def plot_figure(root_list,metrics_list):
     current_time = time.asctime(now).replace(' ','_')
     fig = plt.figure()
     ax1 = plt.axes(projection='3d')
-    x_list = []
-    y_list = []
-    z_list = []
+    u_list = []
+    v_list = []
+    w_list = []
     for root in root_list:
-        x_list.append(root[0])
-        y_list.append(root[1])
-        z_list.append(root[2])
-    xd = np.array(x_list)
-    yd = np.array(y_list)
-    zd = np.array(z_list)
-    ax1.scatter3D(xd,yd,zd, cmap='Blues')
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('y')
-    ax1.set_zlabel('z')
+        u_list.append(root[0])
+        v_list.append(root[1])
+        w_list.append(root[2])
+    ud = np.array(u_list)
+    vd = np.array(v_list)
+    wd = np.array(w_list)
+    ax1.scatter3D(ud,vd,wd, cmap='Blues')
+    ax1.set_xlabel('u')
+    ax1.set_ylabel('v')
+    ax1.set_zlabel('w')
+    ax1.set_title('possible false negative rate root')
     # ax1.plot_surface(xd,yd,zd,rstride=0.005,cstride=0.005,cmap=plt.cm.cool)
     plt.savefig('./scatter3D_demo-{}.jpeg'.format(current_time))
 
